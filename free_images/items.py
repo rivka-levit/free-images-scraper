44782f2b603f6531a4bfd1ca388aad5669620870
rivka-yuliia <juliacreadora@gmail.com>
2024-05-26 15:35:52 +0300

    image item

--- free_images/items.py
+++ free_images/items.py
@@ -7,6 +7,5 @@ import scrapy
 
 
 class FreeImagesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
