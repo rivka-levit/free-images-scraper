b82c16fb68bf4c7e0ce94ed87396ac1d0540231f
rivka-yuliia <juliacreadora@gmail.com>
2024-05-27 18:23:58 +0300

    insert category in command line

--- free_images/spiders/images.py
+++ free_images/spiders/images.py
@@ -6,9 +6,17 @@ from free_images.items import FreeImagesItem
 
 
 class ImagesSpider(CrawlSpider):
+    def __init__(self, *args, **kwargs):
+        super(ImagesSpider, self).__init__(*args, **kwargs)
+        self.category = [kwargs.get('category')]
+
+        # Combined url to insert a category in command line.
+        # Command line: scrapy crawl images -a category=category_name
+        self.start_urls = [f'https://www.freeimages.com/search/{self.category}']
+
     name = "images"
     allowed_domains = ["freeimages.com"]
-    start_urls = ["https://www.freeimages.com/search/cat"]
+    # start_urls = ["https://www.freeimages.com/search/cat"]
     rules = (
         Rule(
             LinkExtractor(
